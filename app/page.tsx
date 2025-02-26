a0c7147215ca247c16ca7a475dce00a027052caa
lorenzomarques <lorenzo.marques246@gmail.com>
2025-02-26 19:06:53 -0300

    remove:main & footer text

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ export default function Home() {
       <Header />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        main
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        footer
       </footer>
     </div>
   );

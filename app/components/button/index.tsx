f620db0496aa1aa4f597ba5cda43d6db168873d7
lorenzomarques <lorenzo.marques246@gmail.com>
2025-03-01 03:12:29 -0300

    fix/deploy

--- app/components/button/index.tsx
+++ app/components/button/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 interface ButtonProps {
-    onclick?: Function;
+    onclick?: () => void;
     text?: string;
     className?: string;
 }
@@ -9,7 +9,7 @@ interface ButtonProps {
 export function Button({ onclick, text, className }: ButtonProps) {
     return <button
         className={`bg-[--primary-color] text-[--secondary-font-color] rounded-lg p-[10px] ${className}`}
-        onClick={() => { onclick ? onclick : {} }}>
+        onClick={onclick}>
         {text}
     </button>
 }
